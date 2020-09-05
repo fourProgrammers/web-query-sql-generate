70a0d23d31bcb06a75885f1d9aad6094514fa7fe
samieel <sotton.maia@gmail.com>
2020-09-05 16:40:57 -0300

    set colors on p, h1, h2 and h3

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import './App.css';
 
-export default (props) => (
+export default (props) => {
 
-  <div>
-    <h1>Dracula theme colors</h1>
-  </div>
-);
+  return (
+    <div>
+      <h1>Dracula Theme Colors</h1>
+      <h2>salve h2</h2>
+      <h3>salve h3</h3>
+      <p>salve no p</p>
+    </div>
+  );
+};
