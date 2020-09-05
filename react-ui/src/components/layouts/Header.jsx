7d372ded1f243b63bbb090fa8b54c795f1e5c5a3
samieel <sotton.maia@gmail.com>
2020-09-05 20:02:27 -0300

    update style

--- react-ui/src/components/layouts/Header.jsx
+++ react-ui/src/components/layouts/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import './Header.css';
 
 export default (props) => {
@@ -7,6 +8,7 @@ export default (props) => {
     return (
         <div className="Header">
             <h2>SQL Query Generator</h2>
+
             <div className="HeaderMenu">
                 <div className="HeaderCard DB">
                     <div className="CardTitle">
@@ -17,11 +19,13 @@ export default (props) => {
                         <a className="CardItem" href="#bdDrop">Drop</a>
                     </div>
                 </div>
+
                 <div className="HeaderCard TB">
                 <div className="CardTitle">
                         Table operations
                     </div>
                     <div className="CardItens">
+                        
                         <a className="CardItem" href="#bdCreate">Create</a>
                         <a className="CardItem" href="#bdDrop">Drop</a>
                         <a className="CardItem" href="#bdDrop">Insert</a>
@@ -29,6 +33,7 @@ export default (props) => {
                         <a className="CardItem" href="#bdDrop">Alter</a>
                     </div>
                 </div>
+                
                 <div className="HeaderCard SL">
                 <div className="CardTitle">
                         Select operations
